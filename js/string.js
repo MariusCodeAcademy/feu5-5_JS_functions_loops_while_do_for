d96e7d61e66261c8b4be634a6f58e03a5ca21da9
Marius CA <marius@example.com>
2023-02-10 16:58:48 +0200

    generateLis

--- js/string.js
+++ js/string.js
@@ -1,14 +1,40 @@
 'use strict';
 console.log('string.js file was loaded');
 
-let stringRezult = '';
+function printInLine() {
+  let stringRezult = '';
 
-for (let i = 0; i < 10; i++) {
-  console.log(i);
-  stringRezult = stringRezult + `${i}, `;
-  // printToHtml('this is for loop');
-  // TODO jei tai paskutinis ciklas deti '.' be tarpelio
+  for (let i = 0; i < 10; i++) {
+    console.log(i);
+    stringRezult = stringRezult + `${i}, `;
+    // printToHtml('this is for loop');
+    // TODO jei tai paskutinis ciklas deti '.' be tarpelio
+  }
+  console.log('stringRezult ===', stringRezult);
+
+  printToHtml(stringRezult);
 }
-console.log('stringRezult ===', stringRezult);
 
-printToHtml(stringRezult);
+{
+  /* <li>1</li>
+<li>2</li>
+<li>3</li> */
+}
+
+function generateLis() {
+  let htmlString = '';
+  console.time('sar1');
+  for (let i = 0; i < 10; i++) {
+    console.log(`<li>item ${i + 1}</li>`);
+    htmlString = htmlString + `<li>item ${i + 1}</li>`;
+  }
+  console.log('htmlString ===', htmlString);
+  document.getElementById('list').innerHTML = htmlString;
+  console.timeEnd('sar1');
+}
+generateLis();
+
+let prikiniai = ['duona', 'sokoladuka', 'marskinius'];
+prikiniai[0];
+prikiniai[1];
+prikiniai[2];
