1fdb27e3e1a39e2179aa319434c73b54bf924446
Marius CA <marius@example.com>
2023-02-10 11:05:34 +0200

    uzd

--- js/for.js
+++ js/for.js
@@ -15,3 +15,8 @@ for (let i = 0; i < 10; i++) {
   console.log('this is for loop');
   // printToHtml('this is for loop');
 }
+// for
+// 1. atspausdinti skaicius nuo 14 iki 35
+// 2. atspausdinti skaicius nuo 0 iki 100, kas trecia skaiciu
+// 3. atspausdinti skaicius nuo 50 iki 24
+// 4. atspausdinti 15 random sveiku skaiciu nuo 1 iki 10
