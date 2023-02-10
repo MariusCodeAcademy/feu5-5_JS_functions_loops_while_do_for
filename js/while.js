09b23a521c3eb3e190eac91f2ff0c4a9369c2e75
Marius CA <marius@example.com>
2023-02-10 10:48:09 +0200

    while

--- js/while.js
+++ js/while.js
@@ -26,6 +26,7 @@ let i = 0;
 
 while (i < 10) {
   console.log(i + 1, 'Js is easy or not');
+  // printToHtml(`${i + 1} Js is easy or not`);
   // ciklo viduje turi buti kazkas keiciama kas galiausiai padaryts condition i false
   i = i + 1;
 }
@@ -34,10 +35,33 @@ while (i < 10) {
 i = 10;
 while (i > 0) {
   console.log('i ===', i);
-  printToHtml(i, 'app');
+  // printToHtml(i, 'app', 'h3');
   i--;
 }
 
 // 1. atspausdinti konsoleje skaicius nuo 45 iki 58
 
+i = 45;
+
+while (i <= 58) {
+  // printToHtml(i);
+  i++;
+}
+
 // 2. atspusdinti skaicius nuo 11 iki 79 kas trecia skaiciu
+
+i = 0;
+
+while (i < 80) {
+  // if (i % 3 === 0) {
+  //   printToHtml(i);
+  // }
+  printToHtml(i);
+  console.log('i ===', i);
+  // i = i + 3;
+  i += 3;
+  // i++;
+}
+
+// generuojam random skaiciu nuo 1 iki 12.
+// kiek kartu turim mesti kauliukius kad iskristu 12 ?
