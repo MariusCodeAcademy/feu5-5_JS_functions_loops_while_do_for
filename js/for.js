ccfc8f2828a93443c92e4004c592941619070210
Marius CA <marius@example.com>
2023-02-10 11:03:19 +0200

    for

---
+++ js/for.js
@@ -0,0 +1,17 @@
+'use strict';
+console.log('for.js file was loaded');
+
+/*
+
+for (begin; condition; step) {
+  // ... loop body ...
+}
+
+*/
+
+// atspausdinti teksta 10 kartu
+// debugger;
+for (let i = 0; i < 10; i++) {
+  console.log('this is for loop');
+  // printToHtml('this is for loop');
+}
